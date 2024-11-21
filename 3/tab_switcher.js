a36966d0764981d8916a84324aba74d889dd35e2
Pavel Korobov <korobovpvl@gmail.com>
2024-11-21 23:24:08 +0300

    task 3

--- 3/tab_switcher.js
+++ 3/tab_switcher.js
@@ -1,8 +1,18 @@
 addEventListener("DOMContentLoaded", () => {
-    
-
-
     for (let tabPanel of document.getElementsByClassName("tabs")) {
-        let tabs = tabPanel.querySelectorAll(".tab");
+        const tabs = tabPanel.querySelectorAll(".tab");
+        const contents = tabPanel.querySelectorAll(".tab__content");
+
+        for (let tab of tabs) {
+            tab.addEventListener("click", (event) => {
+                const tabIndex = Array.from(tabs).indexOf(event.target);
+
+                tabPanel.querySelector(".tab.tab_active").className = "tab";
+                tabPanel.querySelector(".tab__content.tab__content_active").className = "tab__content";
+    
+                contents[tabIndex].className = "tab__content tab__content_active";
+                event.target.className = "tab tab_active";
+            });
+        }
     }
 })
